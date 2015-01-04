c0496abc7d2f2186722b100933fa2aaf01c6aa23
Armin <armin.bart@xpecto.com>
2015-01-04 22:34:30 +0100

    Include file needed by PHP scripts

---
+++ global.php
@@ -0,0 +1,30 @@
+<?php
+
+function get_sql_param($name)
+{
+	$value = trim($_POST[$name]);
+
+//	echo "$name = '$value'<br>";
+
+//	if (strpos($value, "'") !== false)
+//		die("The parameter '$name' contains the illegal character ' !");
+
+	if (strpos($value, ";") !== false)
+		die("The parameter '$name' contains the illegal character ';' !");
+		
+	return $value;
+}
+
+function connect_to_db()
+{
+	$host   = $_POST["host"];
+	$user   = $_POST["user"];
+	$pwd    = $_POST["pwd"];
+	$db     = $_POST["db"];
+
+	$con = mysql_connect($host, $user, $pwd) or die("Connection to host $host as $user failed!");
+
+	mysql_select_db($db) or die("Connection to database $db failed!");
+}
+
+?>
