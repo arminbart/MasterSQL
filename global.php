e541f30964212f0a85b26806968b9cfa511cad79
Armin <Armin.Bart@xpecto.com>
2019-01-02 12:07:38 +0100

    Read db parameters from params file

--- global.php
+++ global.php
@@ -1,9 +1,10 @@
 <?php
 
+ini_set("auto_detect_line_endings", true);
+
 function get_sql_param($name)
 {
 	$value = trim($_POST[$name]);
-
 
 //	echo "$name = '$value'<br>";
 
@@ -16,12 +17,37 @@ function get_sql_param($name)
 	return $value;
 }
 
+function get_php_param($name)
+{
+	$value = trim($_POST[$name]);
+
+	if ($value == "")
+	{
+
+		$file = fopen("params.txt", "r");
+
+		while ($value == "" and !feof($file))
+		{
+			$line = fgets($file);
+echo $line . "<br>";
+			$values = explode("=", $line);
+			if (trim($values[0]) == $name)
+				$value = $values[1];
+		}
+
+		fclose($file);
+	}
+
+	echo "return $name=$value<br>";
+	return $value;
+}
+
 function connect_to_db()
 {
-	$host   = $_POST["host"];
-	$user   = $_POST["user"];
-	$pwd    = $_POST["pwd"];
-	$db     = $_POST["db"];
+	$host   = get_php_param($_POST["host"]);
+	$user   = get_php_param($_POST["user"]);
+	$pwd    = get_php_param($_POST["pwd"]);
+	$db     = get_php_param($_POST["db"]);
 
 	$con = mysql_connect($host, $user, $pwd) or die("Connection to host $host as $user failed!");
 
